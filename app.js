9e6e26bd38322362662ba0e69281f689b1290cbd
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-04 19:33:41 -0300

    feat: build main server

---
+++ app.js
@@ -0,0 +1,20 @@
+const app = require('fastify')({
+    logger: true
+});
+const axios = require('axios');
+const telegramBot = require('node-telegram-bot-api');
+const bot = require('./controllers/insta');
+const insta = require('./controllers/bot');
+
+
+//run the server
+const start = async () => {
+    try {
+        await app.listen({ port: 3000 });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1)
+    }
+}
+
+start()
