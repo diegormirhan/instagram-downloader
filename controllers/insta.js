a1c3b7f1860fd35668b8522c6ef51e1cbb5eb40b
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-09-04 19:34:40 -0300

    feat: build instagram scrapper

---
+++ controllers/insta.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+require('dotenv').config();
+
+//API
+const APIKey = process.env.RAPID_API_KEY
+const APIHost = process.env.RAPID_API_HOST
+
+//Scrapping data from Instagram
+const instaScrapper = async (url) => {
+  const options = {
+    method: 'GET',
+    url: 'https://instagram-looter2.p.rapidapi.com/post-dl',
+    params: {
+      link: url
+    },
+    headers: {
+      'X-RapidAPI-Key': APIKey,
+      'X-RapidAPI-Host': APIHost
+    }
+  };
+
+    try {
+      const response = await axios.request(options);
+        return response.data.data;
+    } catch (error) {
+        return error;
+    }
+  }
+
+module.exports = instaScrapper;
